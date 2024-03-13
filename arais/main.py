a47ed10ea9340ee4cce91eae33096c152d0d5ae1
Sofimpress <146110214+Sofimpress@users.noreply.github.com>
2024-03-14 01:13:55 +0300

    Add files via upload

---
+++ arais/main.py
@@ -0,0 +1,51 @@
+import pygame
+import sys
+from settings import *
+from car import Car
+from obs import Obstacle
+# создание объектов
+pygame.init()
+screen = pygame.display.set_mode((SC_WIDTH, SC_HEIGHT, ))
+clock = pygame.time.Clock()
+
+car = Car(SC_WIDTH//2, SC_HEIGHT//2, 'car.png', pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN)
+
+obs1 = Obstacle("car.png")
+# главный цикл
+while True:
+    # задержка
+    clock.tick(FPS)
+    # цикл обработки событий
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    # изменение объектов, update
+
+    car.update()
+    obs1.update()
+
+    screen.fill(BLACK)
+    car.draw(screen)
+    obs1.draw(screen)
+    pygame.display.update()
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+    
